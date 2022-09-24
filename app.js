79b1906759008c68b5bf8072c7e75112476edaed
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2022-09-23 20:48:13 -0400

    generate webpage

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
+const fs = require('fs');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const generatePage = (Name: Diego, Github: DiegoServe) => {
+    return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
 
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
 };
+  
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+    console.log('Portfolio complete! Check out index.html to see the output!');
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+console.log(generatePage(name, github));
+
+
+  });
+  
+  module.exports = generatePage;
